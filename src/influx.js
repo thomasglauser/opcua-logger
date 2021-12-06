bc04daf1352b600cdc3c766388ca215465fd0065
Thomas Glauser <thomas.glauser01@protonmail.ch>
2021-12-06 18:35:44 +0100

    initial rewrite influx-client to v2

--- src/influx.js
+++ src/influx.js
@@ -1,42 +1,46 @@
-let Influx = require('influx')
-let log = require('log4js').getLogger('influx')
+const { InfluxDB } = require('@influxdata/influxdb-client');
+const { PingAPI } = require('../packages/apis');
+const { url, token, org, bucket } = require('./env');
+const log = require('log4js').getLogger('influx');
 
-let INFLUX = null
+const INFLUX = new InfluxDB({ url, token }).getWriteApi(org, bucket, 'ns');
+const PING = new PingAPI(INFLUX);
 
-async function start (url) {
-  INFLUX = new Influx.InfluxDB(url)
-  let host = (await INFLUX.ping(5000))[0]
-  if (host.online) {
-    log.info(`${host.url.host} responded in ${host.rtt}ms running ${host.version}`)
-  } else {
-    log.warn(`${host.url.host} is offline :(`)
-  }
+async function start() {
+    PING.getPing()
+        .then(() => {
+            log.info('InfluxDB connected');
+        })
+        .catch((error) => {
+            log.warn(error);
+            log.warn('InfluxDB is not responding');
+        });
 }
 
-async function write (points) {
-  let pts = points.map((p) => {
-    let tags = p.tags || {}
-    tags.status = p.status
+async function write(points) {
+    let pts = points.map((p) => {
+        let tags = p.tags || {};
+        tags.status = p.status;
 
-    let fields = { value: p.value }
-    if (p.datatype === 'boolean') fields.value_num = p.value * 1
-    return {
-      measurement: p.measurement,
-      tags,
-      fields,
-      timestamp: p.timestamp
-    }
-  })
+        let fields = { value: p.value };
+        if (p.datatype === 'boolean') fields.value_num = p.value * 1;
+        return {
+            measurement: p.measurement,
+            tags,
+            fields,
+            timestamp: p.timestamp,
+        };
+    });
 
-  try {
-    await INFLUX.writePoints(pts)
-  } catch (e) {
-    if (e.message.includes('partial write')) {
-      log.warn(e.message)
-      return
+    try {
+        INFLUX.writePoints(pts);
+    } catch (e) {
+        if (e.message.includes('partial write')) {
+            log.warn(e.message);
+            return;
+        }
+        log.error(e.message);
+        throw e;
     }
-    log.error(e.message)
-    throw e
-  }
 }
-module.exports = { start, write }
+module.exports = { start, write };
